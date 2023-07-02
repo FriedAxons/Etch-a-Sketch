32daaf58591e09702f8ac180c3216f5dfdd95d77
FriedAxons <remo453@gmail.com>
2023-07-02 02:30:19 -0400

    Create square divs and style project
    This commit begins the project by creating a 16x16 grid of square divs
    dynamically using JavaScript. Then styling was applied to the background
    and the elements inside of the container. Mostly this was in order to
    see the square divs.

---
+++ index.js
@@ -0,0 +1,10 @@
+const container = document.getElementById("container");
+const gridSize = 16;
+
+for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+        const square = document.createElement("div");
+        square.classList.add("grid-square");
+        container.appendChild(square);
+    }
+}
