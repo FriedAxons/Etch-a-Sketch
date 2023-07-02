3402654659311ac0ac809c1b66bd3a12d80572ce
FriedAxons <remo453@gmail.com>
2023-07-02 12:25:27 -0400

    Add hover trail effect to project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const container = document.getElementById("container");
 const gridSize = 16;
 
+// Create the grid
 for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
         const square = document.createElement("div");
@@ -8,3 +9,12 @@ for (let i = 0; i < gridSize; i++) {
         container.appendChild(square);
     }
 }
+
+// Add hover effect to the grid squares
+const gridSquares = document.querySelectorAll(".grid-square");
+
+gridSquares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.classList.add("hovered");
+    });
+});
