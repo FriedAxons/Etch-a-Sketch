c63d146772e3ec977704181a2aa9090f975bb01c
FriedAxons <remo453@gmail.com>
2023-07-03 02:19:35 -0400

    Begin adding button functionality
    There is still some kinks to work out with the functionality of the
    grid button. However the logic has been implemented that enables it to
    prompt the user for a new number limited to between 1 and 100, that will
    generate a new grid of square divs for both rows and columns with the
    number entered.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const container = document.getElementById("container");
-const gridSize = 16;
+// Set the initial grid size
+let gridSize = 16;
 
-// Create the grid
-for (let i = 0; i < gridSize; i++) {
-    for (let j = 0; j < gridSize; j++) {
+// Get references to the container and reset button elements
+const container = document.getElementById("container");
+const resetButton = document.getElementById("reset-button");
+
+// Function to generate the grid
+function generateGrid(numSquares) {
+    // Set the grid template columns and rows based on the number of squares
+    container.style.gridTemplateColumns = `repeat(${numSquares}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${numSquares}, 1fr)`;
+
+    // Generate grid squares
+    for (let i = 0; i < numSquares * numSquares; i++) {
         const square = document.createElement("div");
         square.classList.add("grid-square");
         container.appendChild(square);
@@ -11,10 +20,27 @@ for (let i = 0; i < gridSize; i++) {
 }
 
 // Add hover effect to the grid squares
-const gridSquares = document.querySelectorAll(".grid-square");
-
-gridSquares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        square.classList.add("hovered");
-    });
+container.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("grid-square")) {
+        event.target.classList.add("hovered");
+    }
 });
+
+// Event listener for the reset button
+resetButton.addEventListener("click", () => {
+    // Prompt the user to enter the number of squares per side
+    let input = prompt("Enter the number of squares per side (1-100):");
+    let numSquares = parseInt(input);
+
+    // Check if the input is within the valid range
+    if (numSquares >= 1 && numSquares <= 100) {
+        // Clear the existing grid
+        container.innerHTML = "";
+
+        // Generate a new grid with the specified number of squares
+        generateGrid(numSquares);
+    }
+});
+
+// Initial grid generation
+generateGrid(gridSize);
